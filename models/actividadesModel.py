75898be493b18eefa67879baca42b641618fe1bc
LeonardoBG2003 <148294983+LeonardoBG2003@users.noreply.github.com>
2025-06-01 17:51:39 -0600

    Mejora de actividades
    Ahora se le puede asignar el tutor a la actividad

--- models/actividadesModel.py
+++ models/actividadesModel.py
@@ -1,32 +1,34 @@
 from pydantic import BaseModel
-from typing import Optional, Literal
-from datetime import datetime
+from typing import List, Optional, Literal
+
+class TutorInfo(BaseModel):
+    id: str
+    nombre: str
 
 class ActividadInsert(BaseModel):
     nombre: str
     descripcion: str
-    estatus: Literal["Por realizar", "Cancelada", "Realizada"]
-    obligatoria: bool | None = False
+    estatus: Literal["Por realizar", "Realizada", "Cancelada"]
+    obligatoria: bool
+    tutor_id: str | None = None  # ID del tutor para crear/actualizar
+
+class TutorAsignacion(BaseModel):
+    tutor_id: str
+
+class ActividadSelectID(BaseModel):
+    id: str
+    nombre: str
+    descripcion: str
+    estatus: Literal["Por realizar", "Realizada", "Cancelada"]
+    obligatoria: bool
+    tutor: TutorInfo | None = None  # ID del tutor para crear/actualizar
 
 class Salida(BaseModel):
     estatus: str
     mensaje: str
 
-class ActividadSelect(BaseModel):
-    nombre: str
-    descripcion: str
-    estatus: str
-    obligatoria: bool
-
 class ActividadesSalida(Salida):
-    actividades: list[ActividadSelect]
-
-class ActividadSelectID(BaseModel):
-    _id: str
-    nombre: str
-    descripcion: str
-    estatus: str
-    obligatoria: bool
+    actividades: List[ActividadSelectID]
 
 class ActividadesSalidaID(Salida):
-    actividad: ActividadSelectID | None = None
+    actividad: Optional[ActividadSelectID] = None

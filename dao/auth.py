ac557c78fd1d8de116c7fd7d383debeae84cdca3
Manuel Ramírez <l21010310@zamora.tecnm.mx>
2025-06-01 13:50:46 -0600

    Implementación de accesos explícitos en las consultas

--- dao/auth.py
+++ dao/auth.py
@@ -15,12 +15,14 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/usuarios/login")
 
+
 # Generar token
 def create_access_token(data: dict, expires_delta: timedelta = None):
     to_encode = data.copy()
     expire = datetime.utcnow() + (expires_delta or timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
 
 # Obtener usuario desde token
 def get_current_user(token: str = Depends(oauth2_scheme)):
@@ -44,6 +46,7 @@ def get_current_user(token: str = Depends(oauth2_scheme)):
         raise credentials_exception
     return user
 
+
 # Validación de acceso a rutas explícitas de registro de usuarios Alumno-Tutor-Coordinador (útil para varios roles)
 
 def require_roles(roles_permitidos: list[str]):
@@ -54,7 +57,9 @@ def require_roles(roles_permitidos: list[str]):
                 detail=f"Acceso solo permitido a usuarios tipo: {', '.join(roles_permitidos)}"
             )
         return current_user
+
     return _verifica_roles
+
 
 # Exclusivo al coordinador
 def require_rol(rol_requerido: str):
@@ -62,17 +67,33 @@ def require_rol(rol_requerido: str):
         if current_user["tipo"] != rol_requerido:
             raise HTTPException(status_code=403, detail=f"Acceso solo permitido a usuarios tipo '{rol_requerido}'")
         return current_user
+
     return _verifica_rol
 
-# dao/auth.py (Usuarios-Tutor)
-def require_self_or_coordinador(id_objetivo: str, current_user: dict = Depends(get_current_user)):
-    if current_user["tipo"] == "coordinador":
-        return current_user
-    if str(current_user["_id"]) != id_objetivo:
-        raise HTTPException(status_code=403, detail="Solo puedes acceder o modificar tu propia cuenta")
+
+# Para consulta individual sólo si estás logueado:
+def require_same_user(
+        id_usuario: str,
+        current_user: dict = Depends(get_current_user)
+):
+    if str(current_user["_id"]) != id_usuario:
+        raise HTTPException(
+            status_code=403,
+            detail="Solo puedes consultar tu propio usuario"
+        )
     return current_user
 
-# Verificar si es coordinador
+
+# No lo uso ya...
+# dao/auth.py (Usuarios-Tutor)
+# def require_self_or_coordinador(id_objetivo: str, current_user: dict = Depends(get_current_user)):
+#     if current_user["tipo"] == "coordinador":
+#         return current_user
+#     if str(current_user["_id"]) != id_objetivo:
+#         raise HTTPException(status_code=403, detail="Solo puedes acceder o modificar tu propia cuenta")
+#     return current_user
+
+# Verificar si es coordinador para la operación de eliminación
 def require_coordinador(current_user: dict = Depends(get_current_user)):
     if current_user["tipo"] != "coordinador":
         raise HTTPException(status_code=403, detail="Acceso solo permitido a coordinadores")
